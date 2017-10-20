e86e075c8515c804ac74bcb549dd67e41bd22552
xiaoduc <2760522291@qq.com>
2017-10-20 22:52:57 +0800

    add jwt Token Utils

---
+++ src/main/java/cn/magicdu/bookmark/utils/AuthConfirm.java
@@ -0,0 +1,58 @@
+package cn.magicdu.bookmark.utils;
+
+import java.security.Key;
+import java.util.Date;
+
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
+
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.impl.crypto.MacProvider;
+
+public class AuthConfirm {
+	
+	@SuppressWarnings("unused")
+	private String createAuth(String username){
+		Key key = MacProvider.generateKey();
+
+		String compactJws = Jwts.builder()
+		  .setSubject(username)
+		  .signWith(SignatureAlgorithm.HS512, key)
+		  .compact();
+		return compactJws;
+	}
+	
+	
+	
+	private String createJwt(String id, String issuer, String subject, long ttlMillis){
+		//ApiKey apiKey=new ApiKey();
+		 //The JWT signature algorithm we will be using to sign the token
+	    SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+	 
+	    long nowMillis = System.currentTimeMillis();
+	    Date now = new Date(nowMillis);
+	 
+	    //We will sign our JWT with our ApiKey secret
+	    byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(apiKey.getSecret());
+	    Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
+	 
+	    //Let's set the JWT Claims
+	    JwtBuilder builder = Jwts.builder().setId(id)
+	                                .setIssuedAt(now)
+	                                .setSubject(subject)
+	                                .setIssuer(issuer)
+	                                .signWith(signatureAlgorithm, signingKey);
+	 
+	    //if it has been specified, let's add the expiration
+	    if (ttlMillis >= 0) {
+	    long expMillis = nowMillis + ttlMillis;
+	        Date exp = new Date(expMillis);
+	        builder.setExpiration(exp);
+	    }
+	 
+	    //Builds the JWT and serializes it to a compact, URL-safe string
+	    return builder.compact();
+	}
+}
