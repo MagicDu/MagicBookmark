35a8b0dae20a051e5271c95d2a0bb6dcd0b7f549
xiaoduc <2760522291@qq.com>
2017-11-03 23:17:57 +0800

    update Bookmark Query method

---
+++ src/main/java/cn/magicdu/bookmark/pojo/InfoList.java
@@ -0,0 +1,86 @@
+package cn.magicdu.bookmark.pojo;
+
+public class InfoList {
+	private String userid;
+	private String username;
+	private String bookmarkid;
+	private String bookmarkname;
+	private String bookmarkurl;
+	private String bookmarkdes;
+	private int bookmarkstate;
+	private String categoryid;
+	private String categoryname;
+
+	public String getUserid() {
+		return userid;
+	}
+
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getBookmarkid() {
+		return bookmarkid;
+	}
+
+	public void setBookmarkid(String bookmarkid) {
+		this.bookmarkid = bookmarkid;
+	}
+
+	public String getBookmarkname() {
+		return bookmarkname;
+	}
+
+	public void setBookmarkname(String bookmarkname) {
+		this.bookmarkname = bookmarkname;
+	}
+
+	public String getBookmarkurl() {
+		return bookmarkurl;
+	}
+
+	public void setBookmarkurl(String bookmarkurl) {
+		this.bookmarkurl = bookmarkurl;
+	}
+
+	public String getBookmarkdes() {
+		return bookmarkdes;
+	}
+
+	public void setBookmarkdes(String bookmarkdes) {
+		this.bookmarkdes = bookmarkdes;
+	}
+
+	public int getBookmarkstate() {
+		return bookmarkstate;
+	}
+
+	public void setBookmarkstate(int bookmarkstate) {
+		this.bookmarkstate = bookmarkstate;
+	}
+
+	public String getCategoryid() {
+		return categoryid;
+	}
+
+	public void setCategoryid(String categoryid) {
+		this.categoryid = categoryid;
+	}
+
+	public String getCategoryname() {
+		return categoryname;
+	}
+
+	public void setCategoryname(String categoryname) {
+		this.categoryname = categoryname;
+	}
+
+}
